1951676f227c1979d99e71d6bff088f09b091ed0
Daniel Palmieri <dlspalmieri@gmail.com>
2024-04-19 09:15:53 -0300

    prefix variable names

--- src/Traits/HasSlugToken.php
+++ src/Traits/HasSlugToken.php
@@ -10,19 +10,19 @@ trait HasSlugToken
      * The length of the slug.
      * If null, the default value from the config file will be used.
      */
-    public static ?int $length = null;
+    public static ?int $tokenLength = null;
 
     /**
      * The characters to use for the slug.
      * If null, the default value from the config file will be used.
      */
-    public static ?string $characters = null;
+    public static ?string $tokenCharacters = null;
 
     /**
      * The column name to use for the slug.
      * If null, the default value from the config file will be used.
      */
-    public static ?string $column = null;
+    public static ?string $tokenColumn = null;
 
     /**
      * Boot the trait.
@@ -30,9 +30,9 @@ trait HasSlugToken
     public static function bootHasSlugToken(): void
     {
         static::creating(function (Model $model) {
-            $column = self::$column ?? config('slug-token.column');
-            $length = self::$length ?? config('slug-token.length');
-            $characters = self::$characters ?? config('slug-token.characters');
+            $column = self::$tokenColumn ?? config('slug-token.column');
+            $length = self::$tokenLength ?? config('slug-token.length');
+            $characters = self::$tokenCharacters ?? config('slug-token.characters');
 
             // If the slug is already set, don't generate a new one
             if ($model->{$column}) {
